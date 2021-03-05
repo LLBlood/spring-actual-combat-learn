adfe6e5c9e049ac156782d9813652a39383d2f4d
liulin <liulin@gmail.com>
2021-03-05 17:30:54 +0800

    story# init

---
+++ src/main/java/cn/liulin/springactualcombatlearn/SpringActualCombatLearnApplication.java
@@ -0,0 +1,13 @@
+package cn.liulin.springactualcombatlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringActualCombatLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringActualCombatLearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/liulin/springactualcombatlearn/SpringActualCombatLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.liulin.springactualcombatlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringActualCombatLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
